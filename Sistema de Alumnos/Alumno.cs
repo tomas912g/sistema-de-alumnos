3fdd5e1929fb72c8fb1a8da242253e222a76dbeb
tomas912g <tgutierrezofficial@gmail.com>
2026-08-31 10:19:59 -0300

    etapa 1: creacion clase Alumno

---
+++ Sistema de Alumnos/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema_de_Alumnos
+{
+    public class Alumno
+    {
+        public required string Nombre { get; set; }
+        public int Legajo { get; set; }
+        public decimal Nota1 { get; set; }
+        public decimal Nota2 { get; set; }
+    }
+}
---
+++ Sistema de Alumnos/Program.cs
@@ -0,0 +1,20 @@
+﻿using Sistema_de_Alumnos;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Alumno alumno1 = new Alumno()
+{
+    Nombre = "Augusto",
+    Legajo = 1,
+    Nota1 = 10,
+    Nota2 = 3,
+};
+Alumno alumno2 = new Alumno()
+{
+    Nombre = "Peira",
+    Legajo = 2,
+    Nota1 = 8,
+    Nota2 = 7,
+};
+
+Console.WriteLine($"Alumno: {alumno1.Nombre}. Legajo: {alumno1.Legajo}");
+Console.WriteLine($"Alumno: {alumno2.Nombre}. Legajo: {alumno2.Legajo}");
