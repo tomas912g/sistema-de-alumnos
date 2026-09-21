83a3d4c9d2425e85db03f751a1cbc2bdb5340b47
tomas912g <tgutierrezofficial@gmail.com>
2026-09-21 09:17:22 -0300

    Etapa 6: Menu interactivo

--- Sistema de Alumnos/Program.cs
+++ Sistema de Alumnos/Program.cs
@@ -1,14 +1,98 @@
 ﻿using Sistema_de_Alumnos;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-Alumno alumno1 = new Alumno("Peira", 2);
+List<Alumno> alumnos = new List<Alumno>();
+bool salir = false;
 
-if (alumno1.CargarNotas(8m, 10m) == false);
+while (salir == false)
 {
-    Console.WriteLine("Error: Ingresar una nota valida.");
+    Console.WriteLine("\n--- MENÚ PRINCIPAL ---");
+    Console.WriteLine("1. Agregar alumno");
+    Console.WriteLine("2. Listar alumnos");
+    Console.WriteLine("3. Buscar por legajo");
+    Console.WriteLine("4. Promedio del Curso");
+    Console.WriteLine("5. Alumnos aprobados");
+    Console.WriteLine("6. Salir");
+    Console.Write("Elija una opción: ");
+
+    string opcion = Console.ReadLine();
+
+    if (opcion == "1")
+    {
+        Console.WriteLine("Ingrese el nombre del alumno: ");
+        string nombreAlumno = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el legado del alumno: ");
+        int legajoAlumno = Convert.ToInt32(Console.ReadLine());
+
+        Alumno alumnoNuevo = new Alumno(nombreAlumno, legajoAlumno);
+        alumnos.Add(alumnoNuevo);
+    }
+    else if (opcion == "2")
+    {
+        foreach (Alumno alumnoListados in alumnos)
+        {
+            Console.WriteLine(alumnoListados);
+        };
+    }
+    else if (opcion == "3")
+    {
+        Console.WriteLine("Ingrese el legajo a buscar:");
+        int legajoBuscado = Convert.ToInt32(Console.ReadLine());
+
+        bool encontrado = false;
+
+        foreach (Alumno alumnoEnLista in alumnos)
+        {
+            if (alumnoEnLista.Legajo == legajoBuscado)
+            {
+                Console.WriteLine(alumnoEnLista);
+                encontrado = true;
+            }
+        }
+
+        if (encontrado == false)
+        {
+            Console.WriteLine("Error: No existe alumno con ese legajo");
+        }
+    }
+    else if (opcion == "4")
+    {
+        if (alumnos.Count == 0)
+        {
+            Console.WriteLine("Error: El promedio no se puede calcular");
+        }
+        else
+        {
+            decimal sumaTotal = 0;
+            foreach (Alumno alumnoEnLista in alumnos)
+            {
+                sumaTotal = sumaTotal + alumnoEnLista.Promedio();
+            }
+
+            decimal promedioGeneral = sumaTotal / alumnos.Count;
+            Console.WriteLine($"El promedio general del curso es: {promedioGeneral}");
+        }
+    }
+    else if (opcion == "5")
+    {
+        int cantidadAprobados = 0;
+        foreach (Alumno alumnoEnLista in alumnos)
+        {
+            if (alumnoEnLista.EstaAprobado() == true)
+            {
+                cantidadAprobados = cantidadAprobados + 1;
+            }
+        }
+        Console.WriteLine($"Hay {cantidadAprobados} alumnos aprobados en el currso");
+    }
+    else if (opcion == "6")
+    {
+        salir = true;
+    }
+    
+    else
+    {
+        Console.WriteLine("Opción inexistente, intente de nuevo.");
+    }
 }
-
-Console.WriteLine(alumno1);
-
-//Al hacer un new Alumno() vacio, sale este error porque al definir yo mismo un metodo constructoor el prgrama verifica que cumpla exacetamente con los parametros que tiene que tener (como yo lo defini en el constructor)
-
